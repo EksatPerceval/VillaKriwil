a9caed85462d98e105902b0f9694ebc389617806
Eksa <putueksat@gmail.com>
2026-01-16 22:45:47 +0800

    first

---
+++ script.js
@@ -0,0 +1,33 @@
+// Navbar Menu
+const btnHamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+btnHamburger.addEventListener("click", () => {
+    btnHamburger.classList.toggle("is-active");
+    menu.classList.toggle("menu-active");
+});
+
+menu.addEventListener("click", (event) => {
+    if (event.target.tagName.toLowerCase() === 'a') {
+        menu.classList.remove("menu-active");
+    }
+});
+
+// Feedback
+const loadMore = document.querySelector(".load-more");
+let jumlahItem = 3;
+
+loadMore.addEventListener("click", () => {
+    // console.log("test");
+    let boxes = [...document.querySelectorAll(".feedback-box .box")];
+    for (let i = jumlahItem; i < jumlahItem + 3; i++) {
+        boxes[i].style.display = "inline-block";
+    }
+    jumlahItem += 3;
+
+    if (jumlahItem >= boxes.length) {
+        loadMore.style.display = "none";
+    }
+
+
+});
